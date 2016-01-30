67421d520f799ddb3c8e8a0ce9539e969ab861cb
Ahmed Mostafa <Ahmed Mostafa>
2016-01-30 01:21:03 -0600

    adding participant

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -21,15 +21,56 @@ angular.module('budgetApp')
 		vm.dates.splice(index,1);
   	}
 
-    vm.copyFirstRow = function (){
-      var firstItem = vm.dates[0];
 
-      for (var i = vm.dates.length - 1; i >= 1; i--) {
-        vm.dates[i].fromTime = firstItem.fromTime;
-        vm.dates[i].toDate = firstItem.toDate;
-      };
-
-
+    var self = this;
+    self.querySearch = querySearch;
+    self.allContacts = loadContacts();
+    self.contacts = [];
+    self.filterSelected = true;
+    /**
+     * Search for contacts.
+     */
+    function querySearch (query) {
+      var results = query ?
+          self.allContacts.filter(createFilterFor(query)) : [];
+      return results;
     }
+    /**
+     * Create filter function for a query string
+     */
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+      return function filterFn(contact) {
+        return (contact._lowername.indexOf(lowercaseQuery) != -1);;
+      };
+    }
+    function loadContacts() {
+      var contacts = [
+        'Marina Augustine',
+        'Oddr Sarno',
+        'Nick Giannopoulos',
+        'Narayana Garner',
+        'Anita Gros',
+        'Megan Smith',
+        'Tsvetko Metzger',
+        'Hector Simek',
+        'Some-guy withalongalastaname'
+      ];
+      return contacts.map(function (c, index) {
+        var cParts = c.split(' ');
+        var contact = {
+          name: c,
+          email: cParts[0][0].toLowerCase() + '.' + cParts[1].toLowerCase() + '@example.com',
+          image: 'http://lorempixel.com/50/50/people?' + index
+        };
+        contact._lowername = contact.name.toLowerCase();
+        return contact;
+      });
+    }
+
+
+
+
+
     
   });
